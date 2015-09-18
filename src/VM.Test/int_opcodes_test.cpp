85099595f20bbd90bb6c72be5a9e2e5026bb359f
Joakim Thun <joakimthun@gmail.com>
2015-09-18 06:15:38 +0200

    test refactoring

--- src/VM.Test/int_opcodes_test.cpp
+++ src/VM.Test/int_opcodes_test.cpp
@@ -5,35 +5,36 @@
 
 using namespace elsa::vm;
 
-struct OpCodesSetup
-{
-	static inline VM* execute(std::vector<int>& p)
+class IntOpCodesTest : public testing::Test {
+protected:
+	virtual void SetUp() 
 	{
 		int ep = 0;
 
-		auto vm = new VM(p);
+		vm_.add_constant_entry(new FunctionInfo("main", 0, 0, ep, FunctionType::Static));
+		vm_.set_entry_point(ep);
+	}
 
-		vm->add_constant_entry(new FunctionInfo("main", 0, 0, ep, FunctionType::Static));
-		vm->set_entry_point(ep);
+	virtual void TearDown() {}
 
-		return vm;
-	};
+	VM vm_;
 };
 
-TEST(OPCODES, ICONST)
+TEST_F(IntOpCodesTest, ICONST)
 {
 	std::vector<int> p =
 	{
 		iconst, 6
 	};
 
-	auto vm = std::unique_ptr<VM>(OpCodesSetup::execute(p));
-	vm->execute();
+	vm_.set_program(p);
 
-	EXPECT_EQ(6, vm->eval_stack_top().i());
+	vm_.execute();
+
+	EXPECT_EQ(6, vm_.eval_stack_top().i());
 }
 
-TEST(OPCODES, IADD)
+TEST_F(IntOpCodesTest, IADD)
 {
 	std::vector<int> p =
 	{
@@ -42,13 +43,13 @@ TEST(OPCODES, IADD)
 		iadd
 	};
 
-	auto vm = std::unique_ptr<VM>(OpCodesSetup::execute(p));
-	vm->execute();
+	vm_.set_program(p);
+	vm_.execute();
 
-	EXPECT_EQ(12, vm->eval_stack_top().i());
+	EXPECT_EQ(12, vm_.eval_stack_top().i());
 }
 
-TEST(OPCODES, IMUL)
+TEST_F(IntOpCodesTest, IMUL)
 {
 	std::vector<int> p =
 	{
@@ -57,81 +58,91 @@ TEST(OPCODES, IMUL)
 		imul
 	};
 
-	auto vm = std::unique_ptr<VM>(OpCodesSetup::execute(p));
-	vm->execute();
+	vm_.set_program(p);
+	vm_.execute();
 
-	EXPECT_EQ(36, vm->eval_stack_top().i());
+	EXPECT_EQ(36, vm_.eval_stack_top().i());
 }
 
-TEST(OPCODES, ISUB)
+TEST_F(IntOpCodesTest, ISUB)
 {
-	std::vector<int> p1 =
+	std::vector<int> p =
 	{
 		iconst, 6,
 		iconst, 6,
 		isub
 	};
 
-	auto vm1 = std::unique_ptr<VM>(OpCodesSetup::execute(p1));
-	vm1->execute();
+	vm_.set_program(p);
+	vm_.execute();
 
-	EXPECT_EQ(0, vm1->eval_stack_top().i());
+	EXPECT_EQ(0, vm_.eval_stack_top().i());
+}
 
-	std::vector<int> p2 =
+TEST_F(IntOpCodesTest, ISUB_NEG_VALUE)
+{
+
+	std::vector<int> p =
 	{
 		iconst, 40,
 		iconst, 45,
 		isub
 	};
 
-	auto vm2 = std::unique_ptr<VM>(OpCodesSetup::execute(p2));
-	vm2->execute();
+	vm_.set_program(p);
+	vm_.execute();
 
-	EXPECT_EQ(-5, vm2->eval_stack_top().i());
+	EXPECT_EQ(-5, vm_.eval_stack_top().i());
 }
 
-TEST(OPCODES, IDIV)
+TEST_F(IntOpCodesTest, IDIV)
 {
-	std::vector<int> p1 =
+	std::vector<int> p =
 	{
 		iconst, 12,
 		iconst, 4,
 		idiv
 	};
 
-	auto vm1 = std::unique_ptr<VM>(OpCodesSetup::execute(p1));
-	vm1->execute();
+	vm_.set_program(p);
+	vm_.execute();
 
-	EXPECT_EQ(3, vm1->eval_stack_top().i());
+	EXPECT_EQ(3, vm_.eval_stack_top().i());
+}
 
-	std::vector<int> p2 =
+TEST_F(IntOpCodesTest, IDIV_SAME_VALUE)
+{
+	std::vector<int> p =
 	{
 		iconst, 10,
 		iconst, 10,
 		idiv
 	};
 
-	auto vm2 = std::unique_ptr<VM>(OpCodesSetup::execute(p2));
-	vm2->execute();
+	vm_.set_program(p);
+	vm_.execute();
 
-	EXPECT_EQ(1, vm2->eval_stack_top().i());
+	EXPECT_EQ(1, vm_.eval_stack_top().i());
+}
 
-	std::vector<int> p3 =
+TEST_F(IntOpCodesTest, IDIV_GT)
+{
+	std::vector<int> p =
 	{
 		iconst, 14,
 		iconst, 16,
 		idiv
 	};
 
-	auto vm3 = std::unique_ptr<VM>(OpCodesSetup::execute(p2));
-	vm3->execute();
+	vm_.set_program(p);
+	vm_.execute();
 
-	EXPECT_EQ(1, vm3->eval_stack_top().i());
+	EXPECT_EQ(0, vm_.eval_stack_top().i());
 }
 
-TEST(OPCODES, BR_IEQ)
+TEST_F(IntOpCodesTest, BR_IEQ_JUMP)
 {
-	std::vector<int> p1 =
+	std::vector<int> p =
 	{
 		iconst, 6,
 		iconst, 6,
@@ -139,14 +150,18 @@ TEST(OPCODES, BR_IEQ)
 		halt
 	};
 
-	auto vm1 = std::unique_ptr<VM>(OpCodesSetup::execute(p1));
-	vm1->execute_one();
-	vm1->execute_one();
-	vm1->execute_one();
+	vm_.set_program(p);
+	vm_.execute_one();
+	vm_.execute_one();
+	vm_.execute_one();
 
-	EXPECT_EQ(6, vm1->get_pc());
+	EXPECT_EQ(6, vm_.get_pc());
+}
 
-	std::vector<int> p2 =
+TEST_F(IntOpCodesTest, BR_IEQ_NO_JUMP)
+{
+
+	std::vector<int> p =
 	{
 		iconst, 8,
 		iconst, 6,
@@ -154,10 +169,10 @@ TEST(OPCODES, BR_IEQ)
 		halt
 	};
 
-	auto vm2 = std::unique_ptr<VM>(OpCodesSetup::execute(p2));
-	vm2->execute_one();
-	vm2->execute_one();
-	vm2->execute_one();
+	vm_.set_program(p);
+	vm_.execute_one();
+	vm_.execute_one();
+	vm_.execute_one();
 
-	EXPECT_NE(0, vm2->get_pc());
+	EXPECT_NE(0, vm_.get_pc());
 }
