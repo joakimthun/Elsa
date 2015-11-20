2dadd238c551fedeb7e154a24f19049e5e598f3d
joakimthun <joakimthun@gmail.com>
2015-11-20 06:16:57 +0100

    all tests are now green again

--- src/Compiler.Test/parser_test.cpp
+++ src/Compiler.Test/parser_test.cpp
@@ -30,31 +30,6 @@ BinaryOperatorExpression* assert_is_binary_operator_expression(Expression* exp, 
 	return nullptr;
 }
 
-TEST(ParserTest, PREFIX_OPERATOR_IDENTIFIER)
-{
-	auto lexer = new Lexer(new SourceFile("..\\Compiler.Test\\parser_test_files\\prefix_operator.elsa"));
-	auto parser = ElsaParser(lexer);
-	auto exp = parser.parse_expression();
-
-	if (auto poe = dynamic_cast<PrefixOperatorExpression*>(exp.get()))
-	{
-		ASSERT_EQ(poe->get_operator(), TokenType::Exclamation);
-
-		if (auto ie = dynamic_cast<IdentifierExpression*>(poe->get_right()))
-		{
-			ASSERT_EQ(ie->get_name(), L"my_var");
-		}
-		else
-		{
-			FAIL();
-		}
-	}
-	else
-	{
-		FAIL();
-	}
-}
-
 TEST(ParserTest, VARIABLE_DECLARATION)
 {
 	auto lexer = new Lexer(new SourceFile("..\\Compiler.Test\\parser_test_files\\variable_declaration.elsa"));
