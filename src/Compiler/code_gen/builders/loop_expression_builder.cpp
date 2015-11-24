9fafecd9f776f54f93a2536b5346938646f80554
joakimthun <joakimthun@gmail.com>
2015-11-24 12:34:58 +0100

    for-loops now works

--- src/Compiler/code_gen/builders/loop_expression_builder.cpp
+++ src/Compiler/code_gen/builders/loop_expression_builder.cpp
@@ -7,6 +7,35 @@ namespace elsa {
 
 		void LoopExpressionBuilder::build(VMProgram* program, VMExpressionVisitor* visitor, LoopExpression* expression)
 		{
+			// Declare the loop variable
+			expression->get_variable_expression()->accept(visitor);
+
+			// Set the address to jump to after each iteration
+			auto before_condition_addr_index = program->mark_index();
+
+			// Generate the loop condition
+			expression->get_condition()->accept(visitor);
+
+			// Push true
+			program->emit(OpCode::iconst);
+			program->emit(1);
+
+			// If not equal -> branch
+			program->emit(OpCode::br_ineq);
+			auto after_loop_addr_index = program->mark_index();
+
+			for (auto& exp : expression->get_body())
+			{
+				exp->accept(visitor);
+			}
+
+			expression->get_post_expression()->accept(visitor);
+
+			program->emit(OpCode::br);
+			program->emit(before_condition_addr_index);
+
+			// Set the address to jump to if the loop condition fails
+			program->emit(after_loop_addr_index, program->get_next_instruction_index());
 		}
 
 	}
