8642ebedaeeda249fd7ac7a0e507ca20c0f9104f
joakimthun <joakimthun@gmail.com>
2015-11-26 05:15:05 +0100

    added some basic code gen for array declarations

--- src/Compiler/code_gen/builders/array_declaration_expression_builder.cpp
+++ src/Compiler/code_gen/builders/array_declaration_expression_builder.cpp
@@ -7,7 +7,10 @@ namespace elsa {
 
 		void ArrayDeclarationExpressionBuilder::build(VMProgram* program, VMExpressionVisitor* visitor, ArrayDeclarationExpression* expression)
 		{
-
+			program->emit(OpCode::iconst);
+			program->emit(10);
+			program->emit(OpCode::new_arr);
+			program->emit(static_cast<int>(expression->get_type()->get_vm_type()));
 		}
 
 	}
