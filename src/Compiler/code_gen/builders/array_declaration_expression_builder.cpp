fa8668f8a448eb982c9f6b022e6cb57e612b46d4
joakimthun <joakimthun@gmail.com>
2016-01-05 18:01:43 +0100

    array types now translate correctly

--- src/Compiler/code_gen/builders/array_declaration_expression_builder.cpp
+++ src/Compiler/code_gen/builders/array_declaration_expression_builder.cpp
@@ -10,7 +10,9 @@ namespace elsa {
 			program->emit(OpCode::iconst);
 			program->emit(10);
 			program->emit(OpCode::new_arr);
-			program->emit(static_cast<int>(expression->get_type()->get_vm_type()));
+
+			auto type = expression->get_type()->get_struct_declaration_expression()->get_generic_type();
+			program->emit(static_cast<int>(type->get_vm_type()));
 		}
 
 	}
