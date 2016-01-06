4c619e378a920eeed9a0755b9e1204bb63a1a577
joakimthun <joakimthun@gmail.com>
2016-01-06 15:10:16 +0100

    added support for accessing array elements by index

--- src/Compiler/code_gen/builders/array_access_expression_builder.cpp
+++ src/Compiler/code_gen/builders/array_access_expression_builder.cpp
@@ -7,7 +7,14 @@ namespace elsa {
 
 		void ArrayAccessExpressionBuilder::build(VMProgram* program, VMExpressionVisitor* visitor, ArrayAccessExpression* expression)
 		{
-			
+			auto local = visitor->current_scope()->get_local(expression->get_identifier_expression()->get_name());
+			auto load_inst = local->get_local_type() == LocalType::Local ? OpCode::l_local : OpCode::l_arg;
+
+			program->emit(load_inst);
+			program->emit(static_cast<int>(local->get_index()));
+
+			expression->get_index_expression()->accept(visitor);
+			program->emit(OpCode::l_ele);
 		}
 
 	}
