891b877e958594baa046c732ff2a060fea4321b6
joakimthun <joakimthun@gmail.com>
2016-01-21 22:13:04 +0100

    added support for calling member functions inside other member functions

--- src/Compiler/code_gen/builders/struct_declaration_expression_builder.cpp
+++ src/Compiler/code_gen/builders/struct_declaration_expression_builder.cpp
@@ -10,20 +10,20 @@ namespace elsa {
 		{
 			auto name = expression->get_name();
 			auto struct_info = std::make_unique<StructInfo>(name);
+			auto struct_info_ptr = struct_info.get();
+			program->add_struct(std::move(struct_info));
 
 			for (const auto& field : expression->get_fields())
 			{
-				struct_info->add_field(std::make_unique<FieldInfo>(field->get_name(), field->get_type()->get_vm_type()));
+				struct_info_ptr->add_field(std::make_unique<FieldInfo>(field->get_name(), field->get_type()->get_vm_type()));
 			}
 
 			for (const auto& func : expression->get_functions())
 			{
 				visitor->set_current_type(expression->get_type());
-				struct_info->add_function(FunctionDeclarationExpressionBuilder::build_member(program, visitor, func.get()));
+				struct_info_ptr->add_function(FunctionDeclarationExpressionBuilder::build_member(program, visitor, func.get()));
 				visitor->set_current_type(nullptr);
 			}
-
-			auto index = program->add_struct(std::move(struct_info));
 		}
 
 	}
